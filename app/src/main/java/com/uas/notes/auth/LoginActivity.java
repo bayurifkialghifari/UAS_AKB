57b215c5bc421200e1942279d5dd6be403c0f4cb
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2023-08-04 23:04:25 +0700

    add logout button action

--- app/src/main/java/com/uas/notes/auth/LoginActivity.java
+++ app/src/main/java/com/uas/notes/auth/LoginActivity.java
@@ -59,6 +59,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+            finish();
         }
     }
 
